12d426aa858b555c58c00810fc68d264a9cc393a
Hugo Marroquin <149089365+humarroquin@users.noreply.github.com>
2024-12-07 15:27:45 -0600

    refactored navigation bar

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const btn = document.querySelector('.btn-wrapper');
 const navbar = document.querySelector('.navbar');
 
+// toggle the .show-menu class
 btn.addEventListener('click', () => {
   navbar.classList.toggle('show-menu');
 });
 
-// todo ::after element stays on the page if the menu is left active. Reasearch more about pseudo elements.
+// remove the class .show-menu when the screen size is 661px or bigger
+const mediaQuery = window.matchMedia('(min-width: 661px)');
+
+function handleMediaQueryChange(e) {
+  if (e.matches) {
+    navbar.classList.remove('show-menu');
+  }
+}
+
+mediaQuery.addEventListener('change', handleMediaQueryChange);
